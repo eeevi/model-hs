2ff2dd7647d09de3643da3ae234b56ead7dc31b0
Vulpes Vulpes <50295222+vulpes-solis@users.noreply.github.com>
2021-04-21 19:14:29 +0300

    Just a little practice. Типа это так важно...

---
+++ main.hs
@@ -0,0 +1,20 @@
+-- |
+-- | Personal simple Markov chain model.
+-- | Just for fun (and for practice, ofc.)
+
+module Main where
+
+import ModelM -- | Major functions defined here.
+import Control.Monad (replicateM)
+import System.Random
+
+
+-- | Do some magic here.
+main = do
+    x <- getLine
+    r <- readFile x
+    let s = getAndSplit r
+    n <- getStdGen
+
+    let result = times s (take 10 (randoms n :: [Double]))
+    putStrLn . conn $ result
